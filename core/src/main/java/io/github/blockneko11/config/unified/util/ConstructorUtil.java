f18e149a4e2c6c48cf9884dc14eb302b95df12ff
Block-Mrlimr267 <block_mrlimr267@outlook.com>
2026-02-27 16:47:24 +0800

    ConstructorUtil#newInstance(Class) 更改逻辑

--- core/src/main/java/io/github/blockneko11/config/unified/util/ConstructorUtil.java
+++ core/src/main/java/io/github/blockneko11/config/unified/util/ConstructorUtil.java
@@ -2,10 +2,17 @@ package io.github.blockneko11.config.unified.util;
 
 import io.github.blockneko11.config.unified.exception.ConfigException;
 
+import java.lang.reflect.Constructor;
+
 public final class ConstructorUtil {
     public static <T> T newInstance(Class<T> clazz) throws ConfigException {
         try {
-            return clazz.getDeclaredConstructor().newInstance();
+            Constructor<T> ctor = clazz.getDeclaredConstructor();
+            if (!ctor.isAccessible()) {
+                ctor.setAccessible(true);
+            }
+
+            return ctor.newInstance();
         } catch (ReflectiveOperationException e) {
             throw new ConfigException("Cannot create instance of " + clazz.getName(), e);
         }
