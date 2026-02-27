78411fcafd2aa42fe95864f8a65aa996b62dfe92
Block-Mrlimr267 <block_mrlimr267@outlook.com>
2026-02-27 14:01:37 +0800

    优化逻辑

--- core/src/main/java/io/github/blockneko11/config/unified/conversion/UUIDConvertor.java
+++ core/src/main/java/io/github/blockneko11/config/unified/conversion/UUIDConvertor.java
@@ -1,5 +1,7 @@
 package io.github.blockneko11.config.unified.conversion;
 
+import io.github.blockneko11.config.unified.exception.ConfigConversionException;
+
 import java.util.UUID;
 
 public class UUIDConvertor implements ConfigConvertor<UUID> {
@@ -9,12 +11,16 @@ public class UUIDConvertor implements ConfigConvertor<UUID> {
     }
 
     @Override
-    public UUID toTarget(Object config) {
+    public UUID toTarget(Object config) throws ConfigConversionException {
+        if (!(config instanceof String)) {
+            throw new ConfigConversionException("UUID must be a string");
+        }
+
         return UUID.fromString((String) config);
     }
 
     @Override
-    public Object toSerialized(UUID field) {
+    public Object toSerialized(UUID field) throws ConfigConversionException {
         return field.toString();
     }
 }
